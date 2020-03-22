b6f48ca026bb2d6b46727cfe98ef31c90bc83cae
Hubert Siwkin <hsiwkin@skydistribution.pl>
2020-03-22 19:47:53 +0100

    Initial commit
    Signed-off-by: Hubert Siwkin <hsiwkin@gmail.com>

---
+++ cw2/Controllers/StudentController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using cw2.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace cw2.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public string GetStudents(string orderBy)
+        {
+            return $"Kowalski, Nowak, Lewandowski sortowanie={orderBy}";
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStudent(int id)
+        {
+            if (id == 1)
+            {
+                return Ok("Kowalski");
+            } else if (id == 2)
+            {
+                return Ok("Malewski");
+            }
+
+            return NotFound("Nie znaleziono studenta");
+        }
+
+        [HttpPost]
+        public IActionResult CreateStudent(Student student)
+        {
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok(student);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateStudent(int id, Student student)
+        {
+            student.IdStudent = id;
+            student.IndexNumber = $"s{new Random().Next(1, 20000)}";
+            return Ok("Aktualizacja dokonczona");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteStudent(int id)
+        {
+            return Ok("Usuwanie ukoczone");
+        }
+    }
+}
